6fdc81df7383db1e268774793c36a9cff138d37d
osantamaria <osantamaria@gmail.com>
2025-04-22 16:04:27 -0600

    basic express website completed

--- express_website/app.js
+++ express_website/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Welcome'});
+    res.render('index', {title: 'Computer Not Working'});
 });
 
 app.get('/about', function(req, res) {
@@ -23,6 +23,33 @@ app.get('/about', function(req, res) {
 app.get('/contact', function(req, res) {
     res.render('contact');
 });
+
+/* use app password */
+app.post('/contact/send', function(req, res) {
+    var transporter = nodeMailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'osantamaria@gmail.com',
+            pass: ''
+        }
+    });
+    var mailOptions = {
+        from: 'Oscar Santamaria <osantamaria@gmail.com>',
+        to: 'osantamaria@gmail.com',
+        subject: 'Website Submission',
+        text: 'You have a submission with the following details... Name: ' + req.body.name + 'Email: ' + req.body.email + 'Message: ' + req.body.message,
+        html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+    };
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+            res.redirect('/');
+        } else {
+            console.log('Message Sent: ' + info.response);
+            res.redirect('/');
+        }
+    });
+}); 
 
 app.listen(3000);
 
