0095d1388aa3bf9785cf9d1f37b4e0ad6d42266f
osantamaria <osantamaria@gmail.com>
2025-04-25 15:35:29 -0600

    nodeauth views and layouts

--- nodeauth/app.js
+++ nodeauth/app.js
@@ -1,23 +1,57 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var createError = require('http-errors');
+//const expressValidator = require('express-validator');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//const { ExpressValidator } = require('express-validator');
+//const { default: mongoose } = require('mongoose');
+
 var app = express();
 
-// view engine setup
+/* View Engine Setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/* Handle Sessions */
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+/* Passport */
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- nodeauth/routes/users.js
+++ nodeauth/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* 
+  GET users listing. In this case the '/' is a reference to /users. 
+  If we replace the current '/' with /edit then the route will be /users/edit 
+*/
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* This one is /users/register */
+router.get('/register', function(req, res, next) {
+  /* This one is rendering a view. We need to have a register.jade file */
+  res.render('register', {title: 'Register'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login'});
+});
+
 
 module.exports = router;
