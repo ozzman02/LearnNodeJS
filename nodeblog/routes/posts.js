e1bca0af7f778fd1e18e2c7d769822986e7e2a60
Oscar Santamaria <osantamaria@gmail.com>
2025-05-23 12:36:32 -0600

    nodeblog single post and comments

--- nodeblog/routes/posts.js
+++ nodeblog/routes/posts.js
@@ -8,6 +8,35 @@ var db = require("monk")("localhost/nodeblog");
 const formatError = require("../utils/errorFormatter");
 const { check, validationResult } = require("express-validator");
 
+const validateComment = () => [
+  check("name")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Name field is required"),
+  check("body")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Body field is required"),
+  check("email")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Email field is required")
+    .isEmail()
+    .withMessage("Invalid email format")
+];
+
+router.get("/show/:id", function (req, res, next) {
+  var posts = db.get("posts");
+  posts.findOne({ _id: req.params.id }, function (err, post) {
+    res.render("show", {
+      post: post,
+    });
+  });
+});
+
 router.get("/add", function (req, res, next) {
   var categories = db.get("categories");
   categories.find({}, {}, function (err, categories) {
@@ -22,6 +51,69 @@ router.post(
   "/add",
   upload.single("mainimage"),
   [
+    check("title").trim().notEmpty().withMessage("Title field is required"),
+    check("body").trim().notEmpty().withMessage("Body field is required"),
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req).formatWith(formatError);
+    if (!errors.isEmpty()) {
+      return res.render("addpost", { errors: errors.array() });
+    }
+
+    try {
+      const { title, category, body, author } = req.body;
+      const date = new Date();
+      const mainimage = req.file ? req.file.filename : "noimage.jpg";
+
+      const posts = db.get("posts");
+      await posts.insert({ title, body, category, date, author, mainimage });
+
+      req.flash("success", "Post Added");
+      res.redirect("/");
+    } catch (err) {
+      console.error("Error adding post:", err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+router.post("/addcomment", upload.single("mainimage"), validateComment(), async (req, res, next) => {
+  const errors = validationResult(req).formatWith(formatError);
+
+  if (!errors.isEmpty()) {
+    const posts = db.get("posts");
+    const post = await posts.findOne({ _id: req.body.postid });
+
+    return res.render("show", {
+      errors: errors.array(),
+      post,
+    });
+  }
+
+  try {
+    const postid = req.body.postid;
+    const comment = {
+      name: req.body.name,
+      email: req.body.email,
+      body: req.body.body,
+      commentdate: new Date(),
+    };
+
+    const posts = db.get("posts");
+    await posts.update({ _id: postid }, { $push: { comments: comment } });
+
+    req.flash("success", "Comment Added");
+    res.redirect(`/posts/show/${postid}`);
+  } catch (err) {
+    console.error("Error adding comment:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+/*router.post(
+  "/add",
+  upload.single("mainimage"),
+  [
     check("title").notEmpty().withMessage("Title field is required"),
     check("body").notEmpty().withMessage("Body field is required"),
   ],
@@ -32,7 +124,6 @@ router.post(
         errors: errors.array(),
       });
     } else {
-      console.log(req.body.category);
       var title = req.body.title;
       var category = req.body.category;
       var body = req.body.body;
@@ -66,6 +157,62 @@ router.post(
       );
     }
   }
-);
+);*/
+
+/*router.post(
+  "/addcomment",
+  upload.single("mainimage"),
+  validateComment(),
+  function (req, res, next) {
+    const errors = validationResult(req).formatWith(formatError);
+    if (!errors.isEmpty()) {
+      var posts = db.get("posts");
+      posts.findOne({ _id: req.body.postid }, function (err, post) {
+        if (err) {
+          console.error("Error fetching post:", err);
+          return res.status(500).send("Internal Server Error");
+        }
+        res.render("show", {
+          errors: errors.array(),
+          post: post,
+        });
+      });
+    } else {
+      var name = req.body.name;
+      var email = req.body.email;
+      var body = req.body.body;
+      var commentdate = new Date();
+      var postid = req.body.postid;
+
+      var comment = {
+        name: name,
+        email: email,
+        body: body,
+        commentdate: commentdate,
+      };
+
+      var posts = db.get("posts");
+      posts.update(
+        {
+          _id: postid,
+        },
+        {
+          $push: {
+            comments: comment,
+          },
+        },
+        function (err, doc) {
+          if (err) {
+            throw err;
+          } else {
+            req.flash("success", "Comment Added");
+            res.location("/posts/show/" + postid);
+            res.redirect("/posts/show/" + postid);
+          }
+        }
+      );
+    }
+  }
+);*/
 
 module.exports = router;
